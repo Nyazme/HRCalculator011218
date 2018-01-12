f2f05bf431e095b6b94859460815d501e4a4ddd5
Mark <feinmanmarke@gmail.com>
2018-01-12 14:46:48 -0700

    week 1 assessment

---
+++ src/com/company/Day5.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Day5 {
+    public static void main(String[] args) {
+
+//HR Team User
+        // - Greet User
+        //? - Tell them how it works - Use for end user
+        // - User enters info
+        // - - First Name
+        // - - Last Name
+        // - - Age
+        // - - Age + 1
+        // - - Age + 5
+        // - - Age +10
+        //
+        //X - - If yes, repeat
+        //X - - If no, close
+
+        System.out.println("Hello!");
+        System.out.println("Instructions");
+              String [] questions = {"Please enter First Name:",
+                                     "Please enter Last Name:",
+                "Please enter Current Age:",
+
+        };
+
+        }
+    }
+
+
